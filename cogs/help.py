a181dea0a231642e6cc117fc2570a88e1e22f628
Colin Wilson <github@colinwilson.dev>
2021-06-28 09:13:28 -0400

    Added filter to help command

--- cogs/help.py
+++ cogs/help.py
@@ -27,11 +27,13 @@ class Help(commands.Cog, name="help"):
         embed = discord.Embed(title="Help", description="List of available commands:", color=config["success"])
         for i in self.bot.cogs:
             cog = self.bot.get_cog(i.lower())
-            commands = cog.get_commands()
-            command_list = [command.name for command in commands]
-            command_description = [command.help for command in commands]
-            help_text = '\n'.join(f'{prefix}{n} - {h}' for n, h in zip(command_list, command_description))
-            embed.add_field(name=i.capitalize(), value=f'```{help_text}```', inline=False)
+            print(i)
+            if i not in ["owner", "template", "moderation"]:
+                commands = cog.get_commands()
+                command_list = [command.name for command in commands]
+                command_description = [command.help for command in commands]
+                help_text = '\n'.join(f'{prefix}{n} - {h}' for n, h in zip(command_list, command_description))
+                embed.add_field(name=i.capitalize(), value=f'```{help_text}```', inline=False)
         await context.send(embed=embed)
 
 
