d402e0daae47c56bbb159ca4b4b8ae5d5b802889
Colin Wilson <github@colinwilson.dev>
2021-11-05 09:15:59 -0400

    commented more reminders

--- noncommands/reminderLoop.py
+++ noncommands/reminderLoop.py
@@ -11,30 +11,30 @@ else:
 
 class ReminderLoop:
     def __init__(self):
-        self.mydb = mysql.connector.connect(
-            host=config["dbhost"],
-            user=config["dbuser"],
-            password=config["dbpassword"],
-            database=config["databasename"]
-        )
+        # self.mydb = mysql.connector.connect(
+        #     host=config["dbhost"],
+        #     user=config["dbuser"],
+        #     password=config["dbpassword"],
+        #     database=config["databasename"]
+        # )
 
-    async def checkReminders(self, bot):
-        mycursor = self.mydb.cursor(buffered=True)
+    # async def checkReminders(self, bot):
+    #     mycursor = self.mydb.cursor(buffered=True)
 
-        mycursor.execute("SELECT * FROM reminders WHERE remind_time <= NOW()")
+    #     mycursor.execute("SELECT * FROM reminders WHERE remind_time <= NOW()")
 
-        for m in mycursor:
-            for channel in bot.get_all_channels():
-                try:
-                    msg = await channel.fetch_message(m[2])
-                except:
-                    continue
-                try:
-                    await msg.reply("Reminded.")
-                    break
-                except:
-                    pass
+    #     for m in mycursor:
+    #         for channel in bot.get_all_channels():
+    #             try:
+    #                 msg = await channel.fetch_message(m[2])
+    #             except:
+    #                 continue
+    #             try:
+    #                 await msg.reply("Reminded.")
+    #                 break
+    #             except:
+    #                 pass
         
-        mycursor.execute("DELETE FROM reminders WHERE remind_time < NOW()")
+    #     mycursor.execute("DELETE FROM reminders WHERE remind_time < NOW()")
 
-        self.mydb.commit()
+    #     self.mydb.commit()
