8d4b7c70f21c3529b1e829b4dacc05cca53112cc
Colin Wilson <github@colinwilson.dev>
2022-01-16 01:17:42 -0500

    Create antialex.py

---
+++ noncommands/antialex.py
@@ -0,0 +1,18 @@
+import yaml
+import sys
+import os
+import uwuify
+
+if not os.path.isfile("config.yaml"):
+    sys.exit("'config.yaml' not found! Please add it and try again.")
+else:
+    with open("config.yaml") as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
+
+class AntiMayhem:
+    async def gotem(self, context):
+        # Only reply to a specific user
+        if context.author.id == 212384186672218112 and len(context.message.content) >= 100:
+            message = await context.channel.fetch_message(context.message.reference.message_id)
+            flags = uwuify.SMILEY | uwuify.YU
+            await context.reply(uwuify.uwu(message.content, flags=flags))
