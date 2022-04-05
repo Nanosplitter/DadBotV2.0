62bc3f8478f9cbdfde4e44ada3e0c9ce1545572c
Colin Wilson <github@colinwilson.dev>
2022-04-05 11:41:14 -0400

    Change bot to message

--- bot.py
+++ bot.py
@@ -78,8 +78,8 @@ async def on_message(message):
         return
     
     if (message.author.id == 293482309296848896):
-        bot.add_reaction(message, "👀")
-        bot.add_reaction(message, "🤔")
+        message.add_reaction("👀")
+        message.add_reaction("🤔")
     
     await imChecker.checkIm(message)
 
