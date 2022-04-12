e023ca76569d9b834fc7eaa6e20ed39eeb689c54
Colin Wilson <github@colinwilson.dev>
2022-04-12 16:26:07 -0400

    Add color changing

--- cogs/general.py
+++ cogs/general.py
@@ -8,6 +8,7 @@ import dateparser as dp
 import pytz
 import datetime
 from dateparser.search import search_dates
+from colour import Color
 
 import aiohttp
 import discord
@@ -120,6 +121,52 @@ class general(commands.Cog, name="general"):
             text=f"Pong request by {context.message.author}"
         )
         await context.send(embed=embed)
+    
+    def luminance(self, color):
+        """
+        Calculate the luminance of a color.
+        """
+        return (0.299 * Color(color).red + 0.587 * Color(color).green + 0.0722 * Color(color).blue)
+
+    def contrast(self, color1, color2):
+        """
+        Calculate the contrast between two colors.
+        """
+        lum1 = self.luminance(color1)
+        lum2 = self.luminance(color2)
+        return (max(lum1, lum2) + 0.05) / (min(lum1, lum2) + 0.05)
+    
+    @commands.command(name="changecolor")
+    async def changecolor(self, context, color):
+        try:
+            if context.message.guild.id != 856919397754470420 and context.message.guild.id != 850473081063211048:
+                return
+            if self.contrast("#36393f", color) < 3:
+                embed = discord.Embed(
+                    title="Error",
+                    description="Color does not have enough contrast. That color has a contrast ratio of: " + str(round(self.contrast("#36393f", color), 4)) + ". It needs to be above 3.",
+                    color=int(color.replace("#", ""), 16)
+                )
+                await context.send(embed=embed)
+                return
+            userRoles = context.message.author.roles
+
+            if len(userRoles) > 1:
+                topRole = userRoles[-1]
+                await topRole.edit(colour=discord.Colour(int(color.replace("#", ""), 16)))
+                embed = discord.Embed(
+                    title="Success!",
+                    description="Color has been changed!",
+                    color=int(color.replace("#", ""), 16)
+                )
+                await context.send(embed=embed)
+        except:
+            embed = discord.Embed(
+                title="Error",
+                description="Something went wrong, make sure you are using a 6 digit hex code. (ex: !changecolor #FFFFFF)",
+                color=config["error"]
+            )
+            await context.send(embed=embed)
 
     @commands.command(name="remindme")
     async def remindme(self, context, *args):
